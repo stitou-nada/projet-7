05f97c0680e36040bbd67b451d06d648f08af84c
stitou-nada <nadastito1087@gmail.com>
2021-12-09 10:49:48 +0100

    update

--- protoype-A/js.js
+++ protoype-A/js.js
@@ -1,39 +1,39 @@
 document.getElementById("formSubmit").addEventListener("submit", function (event) {
     event.preventDefault();
-    var work = readwork();
-    insertNewRow(work);
+    var ovrage = readOvrage();
+    insertNewRow(ovrage);
 })
 
 
-function readwork() {
+function readOvrage() {
 
-    var work = {};
-    work["title"] = document.getElementById("inputTitle").value;
-    work["author"] = document.getElementById("inputAuthor").value;
-    work["price"] = parseFloat(document.getElementById("inputPrix").value);
-    work["date"] = document.getElementById("inputDate").value;
-    work["language"] = document.getElementById("inputLanguage").value;
-    var cheackValues = document.getElementsByName("workType");
+    var ovrage = {};
+    ovrage["title"] = document.getElementById("inputTitle").value;
+    ovrage["author"] = document.getElementById("inputAuthor").value;
+    ovrage["price"] = parseFloat(document.getElementById("inputPrix").value);
+    ovrage["date"] = document.getElementById("inputDate").value;
+    ovrage["language"] = document.getElementById("inputLanguage").value;
+    var cheackValues = document.getElementsByName("ovrageType");
     for (var i = 0; i < cheackValues.length; i++) {
         if (cheackValues[i].checked) {
-            work["type"] = cheackValues[i].value;
+            ovrage["type"] = cheackValues[i].value;
             break;
         }
     }
-    return work;
+    return ovrage;
 }
 
 
 
-function insertNewRow(work) {
-    var tableBody = document.getElementById("worksTable").getElementsByTagName('tbody')[0];
+function insertNewRow(ovrage) {
+    var tableBody = document.getElementById("ovragesTable").getElementsByTagName('tbody')[0];
     var newRow = tableBody.insertRow(tableBody.length);
-    newRow.insertCell(0).innerHTML = work.title;
-    newRow.insertCell(1).innerHTML = work.author;
-    newRow.insertCell(2).innerHTML = work.price;
-    newRow.insertCell(3).innerHTML = work.date;
-    newRow.insertCell(4).innerHTML = work.language;
-    newRow.insertCell(5).innerHTML = work.type;
+    newRow.insertCell(0).innerHTML = ovrage.title;
+    newRow.insertCell(1).innerHTML = ovrage.author;
+    newRow.insertCell(2).innerHTML = ovrage.price;
+    newRow.insertCell(3).innerHTML = ovrage.date;
+    newRow.insertCell(4).innerHTML = ovrage.language;
+    newRow.insertCell(5).innerHTML = ovrage.type;
     cell7 = newRow.insertCell(6)
 
 
