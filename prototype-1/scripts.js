706811f3dc6f17d4d478a70d9ae70b97bfa55c83
stitou-nada <nadastito1087@gmail.com>
2022-01-17 09:20:11 +0000

    add

--- prototype-1/scripts.js
+++ prototype-1/scripts.js
@@ -9,7 +9,11 @@ document.getElementById("formSubmit").addEventListener("submit", function (event
     if (selectRow == null) {
 
         gestionOuvrage.addOuvrage(ouvrage);
-    } 
+    } else
+    if (confirm("Êtes-vous sûr de modifier cette œuvre?")){
+        ouvrage.id = rowId;
+        gestionOuvrage.editOuvrage(ouvrage)
+    }
 
     insertNewRow();
 
