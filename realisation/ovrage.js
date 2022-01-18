6acccc9a82c436763d9a8724a0947360d354a001
stitou-nada <nadastito1087@gmail.com>
2022-01-18 12:05:57 +0000

    add

--- realisation/ovrage.js
+++ realisation/ovrage.js
@@ -1,52 +1,12 @@
 class Ouvrage{
-    #_titre;
-    #_auteur;
-    #_Prix;
-    #_Date;
-    #_Type;
-    #_Langue;
-    #_id;
-get titre(){
-    return this.#_titre;
-}
-set titre(value){
-    this.#_titre=value;
-}
-get id(){
-    return this.#_id;
-}
-set id(value){
-    this.#_id=value;
-}
-get auteur(){
-    return this.#_auteur;
-}
-set auteur(value){
-    this.#_auteur=value;
-}
-get Prix(){
-    return this.#_Prix;
-}
-set Prix(value){
-    this.#_Prix=value;
-}
-get Date(){
-    return this.#_Date;
-}
-set Date(value){
-    this.#_Date=value;
-}
-get Type(){
-    return this.#_Type;
-}
-set Type(value){
-    this.#_Type=value;
-}
-get Langue(){
-    return this.#_Langue;
-}
-set Langue(value){
-    this.#_Langue=value;
-}
+    id;
+    titre;
+    auteur;
+    prix;
+    date;
+    langue;
+    type;
     
+    
+  
 }
--- realisation/scripts.js
+++ realisation/scripts.js
@@ -27,6 +27,11 @@ document.getElementById("formSubmit").addEventListener("submit", function (event
 
 function resetForm() {
     document.getElementById("inputTitle").value = "";
+    document.getElementById("inputAuthor").value = "";
+    document.getElementById("inputPrix").value = "";
+    document.getElementById("inputDate").value = "";
+    document.getElementById("inputLanguage").value = "";
+    document.querySelector('input[name="workType"]:checked').checked = false
     selectRow = null;
 }
 
@@ -35,17 +40,12 @@ function readOuvrage() {
     var ouvrage = new Ouvrage();
     
     ouvrage.titre = document.getElementById("inputTitle").value;
-    ouvrage.auteur = document.getElementById("inputAuthor").value;
-    ouvrage.Prix =  parseFloat(document.getElementById("inputPrix").value);
-    ouvrage.Date =  document.getElementById("inputDate").value;
-    ouvrage.Langue = document.getElementById("inputLanguage").value;
-    var cheackValues = document.getElementsByName("workType");
-    for (var i = 0; i < cheackValues.length; i++) {
-        if (cheackValues[i].checked) {
-            work["type"] = cheackValues[i].value;
-            break;
-        }
-    }
+    ouvrage["auteur"] = document.getElementById("inputAuthor").value;
+    ouvrage["prix"] = parseFloat(document.getElementById("inputPrix").value);
+    ouvrage["date"]= document.getElementById("inputDate").value;
+    ouvrage["langue"] = document.getElementById("inputLanguage").value;
+    ouvrage["type"] = document.querySelector('input[name="workType"]:checked').value
+
     return ouvrage;
 }
 
@@ -53,6 +53,7 @@ function readOuvrage() {
 
 function insertNewRow() {
     var List = gestionOuvrage.ouvrageList
+    console.log(List)
     var tableBody = document.getElementById("worksTable").getElementsByTagName('tbody')[0];
    while(tableBody.rows.length > 0){
        tableBody.deleteRow(0);
@@ -61,18 +62,25 @@ function insertNewRow() {
     var newRow = tableBody.insertRow(tableBody.length);
     cell1 = newRow.insertCell(0)
     cell1.innerHTML =List[i].id;
+
     cell2 = newRow.insertCell(1);
     cell2.innerHTML = List[i].titre;
+
     cell3 = newRow.insertCell(2);
-    cell3.inserHTML = List[i].auteur;
+    cell3.innerHTML = List[i].auteur;
+  
     cell4 = newRow.insertCell(3);
-    cell4.inserHTML = List[i].Prix;
+    cell4.innerHTML = List[i].prix;
+
     cell5 = newRow.insertCell(4);
-    cell5.inserHTML = List[i].Date;
+    cell5.innerHTML = List[i].date;
+
     cell6 = newRow.insertCell(5);
-    cell6.inserHTML = List[i].Langue;
+    cell6.innerHTML = List[i].langue;
+
     cell7 = newRow.insertCell(6);
-    cell7.inserHTML = List[i].Type;
+    cell7.innerHTML = List[i].type;
+
     cell8 = newRow.insertCell(7);
 
 
@@ -99,11 +107,17 @@ function modifier(buttonReference) {
     var ouvrage = new Ouvrage();
     ouvrage = gestionOuvrage.getItem(rowId)
     document.getElementById("inputTitle").value = ouvrage.titre;
-    // document.getElementById("inputTitle").value = ouvrage.auteur;
-    // document.getElementById("inputTitle").value = ouvrage.Prix;
-    // document.getElementById("inputTitle").value = ouvrage.Date;
-    // document.getElementById("inputTitle").value = ouvrage.Langue;
-    // document.getElementById("inputTitle").value = ouvrage.Type;
+    document.getElementById("inputAuthor").value = ouvrage.auteur;
+    document.getElementById("inputPrix").value = ouvrage.prix;
+    document.getElementById("inputDate").value = ouvrage.date;
+    document.getElementById("inputLanguage").value = ouvrage.langue;
+
+    var checkValue = document.getElementsByName("workType");
+    for (var i = 0; i < checkValue.length; i++) {
+        if (checkValue[i].value == ouvrage.type) {
+            checkValue[i].checked = true
+        }
+    }
 
 }
 function suprimer(buttonReference) {
